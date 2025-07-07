3498c74fb59e7d4a5b4917964010ad9d03b54233
Lina Ramazan <rmdan0519@gmail.com>
2025-07-07 21:10:27 +0300

    ilk commit

---
+++ type.py
@@ -0,0 +1,106 @@
+#lesson2
+
+
+#ex1
+##print(hello world)   error 
+
+
+#ex2
+hello="welcom to python"
+print(hello)   
+
+
+#ex3 get type of variable
+hello="welcom to python"
+print(type(hello)) 
+
+#ex4
+#casting
+# التحويل القسري
+
+num=str(3)
+print(num)
+print("num variable is :",num,"type of num:",type(num))
+
+
+num1=3
+print(num1)
+print("num1 variable is :",num1,"type of num1:",type(num1))
+
+
+num2=int(3)
+print(num2)
+print("num2 variable is :",num2,"type of num2:",type(num2))
+
+print("num2 variable is :")
+print(num2)
+print("type of num2:")
+print(type(num2))
+
+
+
+##float
+x=2.5
+print(x)
+print(type(x))
+
+
+num3=float(3)
+print(num3)
+print("num3 variable is :",num3,"type of num3:",type(num3))
+
+
+
+##ex4
+name="ilays"
+name2='ilays'
+print(name, type(name))
+print(name2, type(name2))
+
+##مثال 5
+#حساسية اللغة
+a=10
+A=20
+print(a)
+print(A)
+
+
+##6 name variabls
+mystud="shereen"
+my_stud="shereen2"
+_my_stud="shereen3"
+myStud="shereen4"
+MYSTUD="shereen"
+myStud2="shereen4"
+
+
+#7 ERORR NAMES
+## 2MYSTUD="MOHAMMAD"
+##my-stud="mohammadf"
+##print(my-stud)
+
+##my stud="13246"
+## ja=5
+##if=10
+
+##8
+myStudNamaa="adel"
+MyStudNamaa="adel"
+my_stud_namaa="adel"
+
+##9
+
+name,age,gender="hamaza","22","male"
+print(name,age,gender)
+##10
+##error
+"""
+name,age,gender2="hamaza","22"
+print(name,age,gender2)
+"""
+##11
+name1,name2,name3="apple","apple","apple"
+print(name1,name2,name3)
+
+name1=name2=name3="apple"
+print(name1,name2,name3)
